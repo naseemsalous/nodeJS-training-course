538a00ef81d4444321fde980adc43e6ae211a7ff
naseem <s12113453@stu.najah.edu>
2023-08-26 19:59:49 +0300

    problem_solving_2

---
+++ prblem_solving_2.js
@@ -0,0 +1,22 @@
+function isPrime(number) {
+    if (number <= 1) {
+      return false; 
+    }
+  
+ 
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false; 
+      }
+    }
+  
+    return true;
+  }
+  
+
+  console.log(isPrime(2));   //  true
+  console.log(isPrime(17));  //  true
+  console.log(isPrime(4));   //  false
+  console.log(isPrime(9));   //  false
+  console.log(isPrime(1));   //  false
+  
